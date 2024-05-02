643082f823c561653df8c716555b9d39d128826c
Lingdujun <yfzhang@hfut.edu.cn>
2024-05-02 15:34:45 +0000

    Full support excel style location
    Signed-off-by: Lingdujun <yfzhang@hfut.edu.cn>

--- xlsx_merge.py
+++ xlsx_merge.py
@@ -6,13 +6,68 @@ from tkinter import END
 from tkinter import filedialog
 import os
 import openpyxl
-#v0.92
-#fix: location_split(A)
-#fix: open xlsx files with data_only
+#v0.99
+# Full support excel style location! @ 2024.5.2
+
+
+## A self_add function for Excel style 26 base num
+# where [A-Z] was used instead of [0-9] and [A-F]
+def self_add_ABC(st):
+    if len(st) == 0:
+        return "A"
+    s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    Inverse_st = st[::-1]
+    for i in range(26):
+        if s[i] == Inverse_st[0]:
+            if i < 25:
+                return_st = st[:-1]+s[i+1]
+                return return_st
+            elif i == 25:
+                return_st = self_add_ABC(st[:-1])+"A"
+                return return_st
+
+## A excel style location tansfrom function
+# Full support excel style
+# eg:
+# A1:4 to A1,A2,A3,A4
+# A2:B4 to A2,A3,A4,B2,B3,B4
+# Replace old "location_split" at 2024.5.2
+def location_split(input_str):
+    A=input_str.split(',')
+    location=[]
+    for j in A:
+        if ':' in j:
+            i=j.split(':')
+            location_start=i[0]
+            location_end=i[1]
+            location_start_split=re.split("([0-9])", location_start, maxsplit=1) 
+            location_start_ABC=location_start_split[0]
+            location_start_NUM=location_start_split[1]
+            if location_end[0] in '1234567890':
+                location_end_ABC=location_start_ABC
+                location_end_NUM=location_end
+            else:
+                location_end_split=re.split("([0-9])", location_end, maxsplit=1)
+                location_end_ABC=location_end_split[0]
+                location_end_NUM=location_end_split[1]
+
+            max_loop_num=1000
+            location_now_ABC=location_start_ABC
+            for loop_num in range(max_loop_num):
+                if int(location_end_ABC,base=36)>= int(location_now_ABC,base=36):
+                    for NUM in list(range(int(location_start_NUM),int(location_end_NUM)+1)):
+                        location.append(location_now_ABC+str(NUM))
+                    location_now_ABC=self_add_ABC(location_now_ABC)          
+            break
+        else:
+            location.append(j)
+    return location
 
 
 #read Merge Location
-#tansfrom “A1:4” to ['A1','A2,'A3','A4]
+# tansfrom “A1:4” to ['A1','A2,'A3','A4]
+# Replaced by new function at 2024.5.2
+'''
 def location_split(A):
     location=[]
     for j in A:
@@ -29,7 +84,7 @@ def location_split(A):
         else:
             location.append(j)
     return location
-
+'''
 
 def main_window():
     root=tk.Tk()
